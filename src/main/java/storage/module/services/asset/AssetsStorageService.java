5f2fe87a787324f896462a4442d7b29e1a2a0a63
Federico Zanardo <federicozanardo98@gmail.com>
2023-08-25 10:59:41 +0200

    remove seeding methods

--- src/main/java/storage/module/services/asset/AssetsStorageService.java
+++ src/main/java/storage/module/services/asset/AssetsStorageService.java
@@ -1,7 +1,6 @@
 package storage.module.services.asset;
 
 import lcp.lib.models.assets.Asset;
-import lcp.lib.models.assets.FungibleAsset;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 import storage.constants.Constants;
@@ -19,35 +18,6 @@ public class AssetsStorageService extends StorageSerializer<Asset> implements IA
 
     public AssetsStorageService() {
         RocksDB.loadLibrary();
-    }
-
-    public void seed() throws RocksDBDatabaseException {
-        String assetId = "stipula_coin_asd345";
-        String aliceAssetId = "stipula_assetA_ed8i9wk";
-        String bobAssetId = "stipula_assetB_pl1n5cc";
-
-        FungibleAsset stipulaCoinConfig = new FungibleAsset("StipulaCoin", "STC", 10000, 2);
-        FungibleAsset assetAConfig = new FungibleAsset("Asset A", "ASA", 10000, 2);
-        FungibleAsset assetBConfig = new FungibleAsset("Asset B", "ASB", 10000, 2);
-
-        Asset stipulaCoin = new Asset(assetId, stipulaCoinConfig);
-        Asset assetA = new Asset(aliceAssetId, assetAConfig);
-        Asset assetB = new Asset(bobAssetId, assetBConfig);
-
-        db = RocksDBUtils.open(String.valueOf(Constants.ASSETS_PATH));
-        try {
-            db.put(bytes(assetId), this.serialize(stipulaCoin));
-            db.put(bytes(aliceAssetId), this.serialize(assetA));
-            db.put(bytes(bobAssetId), this.serialize(assetB));
-        } catch (RocksDBException e) {
-            throw new RuntimeException(e);
-        } finally {
-            db.close();
-        }
-
-        /*System.out.println("seed: assetId => " + assetId);
-        System.out.println("seed: aliceAssetId => " + aliceAssetId);
-        System.out.println("seed: bobAssetId => " + bobAssetId);*/
     }
 
     public Asset getAssetInfo(String assetId) throws AssetNotFoundException, DatabaseException {
--- src/main/java/storage/module/services/ownership/OwnershipsStorageService.java
+++ src/main/java/storage/module/services/ownership/OwnershipsStorageService.java
@@ -1,7 +1,6 @@
 package storage.module.services.ownership;
 
 import lcp.lib.models.ownership.Ownership;
-import lcp.lib.models.singleuseseal.Amount;
 import lcp.lib.models.singleuseseal.SingleUseSeal;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
@@ -25,74 +24,6 @@ public class OwnershipsStorageService extends StorageSerializer<ArrayList<Owners
 
     public OwnershipsStorageService() {
         RocksDB.loadLibrary();
-    }
-
-    public void seed() throws RocksDBDatabaseException {
-        String assetId = "stipula_coin_asd345";
-        String aliceAssetId = "stipula_assetA_ed8i9wk";
-        String bobAssetId = "stipula_assetB_pl1n5cc";
-
-        String aliceAddress = "ubL35Am7TimL5R4oMwm2OxgAYA3XT3BeeDE56oxqdLc=";
-        String bobAddress = "f3hVW1Amltnqe3KvOT00eT7AU23FAUKdgmCluZB+nss=";
-
-        String aliceOwnershipId = "2b4a4614-3bb4-4554-93fe-c034c3ba5a9c";
-        String bobOwnershipId = "7a19f50e-eae9-461d-bd58-9946ea39ccf0";
-        String borrowerOwnershipId = "1ce080e5-8c81-48d1-b732-006fa1cc4e2e";
-
-        Amount amountAliceOwnership = new Amount(1400, 2);
-        Amount amountBobOwnership = new Amount(1100, 2);
-        Amount amountBorrowerOwnership = new Amount(1200, 2);
-
-        db = RocksDBUtils.open(String.valueOf(Constants.OWNERSHIPS_PATH));
-
-        ArrayList<Ownership> funds = null;
-
-        try {
-            funds = this.deserialize(db.get(bytes(aliceAddress)));
-        } catch (Exception exception) {
-            db.close();
-            System.out.println("seed: This address does not have any asset saved in the storage");
-        }
-
-        if (funds == null) {
-            funds = new ArrayList<>();
-        }
-
-        funds.add(new Ownership(aliceOwnershipId, new SingleUseSeal(aliceAssetId, amountAliceOwnership, aliceAddress)));
-        try {
-            db.put(bytes(aliceAddress), this.serialize(funds));
-        } catch (RocksDBException e) {
-            db.close();
-            throw new RuntimeException(e);
-        }
-
-        funds = null;
-
-        try {
-            funds = this.deserialize(db.get(bytes(bobAddress)));
-        } catch (Exception exception) {
-            db.close();
-            System.out.println("seed: This address does not have any asset saved in the storage");
-        }
-
-        if (funds == null) {
-            funds = new ArrayList<>();
-        }
-
-        funds.add(new Ownership(bobOwnershipId, new SingleUseSeal(bobAssetId, amountBobOwnership, bobAddress)));
-        funds.add(new Ownership(borrowerOwnershipId, new SingleUseSeal(assetId, amountBorrowerOwnership, bobAddress)));
-
-        try {
-            db.put(bytes(bobAddress), this.serialize(funds));
-        } catch (RocksDBException e) {
-            throw new RuntimeException(e);
-        } finally {
-            db.close();
-        }
-
-        /*System.out.println("seed: aliceOwnershipId => " + aliceOwnershipId);
-        System.out.println("seed: bobOwnershipId => " + bobOwnershipId);
-        System.out.println("seed: borrowerOwnershipId => " + borrowerOwnershipId);*/
     }
 
     // FIXME: return a boolean (true --> success, false --> otherwise)
